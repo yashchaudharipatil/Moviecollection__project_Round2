8b7a1ba5d7e5c7c65ef3e479d60b717139c31452
yashchaudharipatil <131573695+yashchaudharipatil@users.noreply.github.com>
2023-08-27 11:21:05 +0530

    Update urls.py

--- movie_collection_project/urls.py
+++ movie_collection_project/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('movie_collection.urls')),  # Include the app's URL configuration
-    path('', include('movie_collection.movie_views')),  # Include the app's views if needed
+    path('api/', include('movie_collection.urls')),
+    path('', include('movie_collection.movie_views')),  
     path('api-auth/', include('rest_framework.urls')),
 ]
